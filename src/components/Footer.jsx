d808825bce753ef38ba4d4b89f7d365d6f59bb8a
Sunil <sky57934568@gmail.com>
2024-04-19 11:50:53 +0530

    added attribution

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,11 +10,21 @@ const Footer = () => {
   return (
     <footer>
       <div className="rules">
+        <div className="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="https://github.com/sunilyadav-xs" target="_blank">Sunil Yadav</a>.
+        </div>
+
         <button onClick={handleRules}>Rules</button>
       </div>
       {showRules && (
         <div className="modal" onClick={handleRules}>
-          <span className="close" onClick={handleRules} >&times;</span>
+          <span className="close" onClick={handleRules}>
+            &times;
+          </span>
           <img
             className="modal-content"
             src="./images/image-rules.svg"
